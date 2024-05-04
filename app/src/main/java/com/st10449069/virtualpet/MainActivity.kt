f8229b0c251e29915b855283f45540eb16510b5c
K.Ratsoma <ST10449069@vcconnect.edu.za>
2024-05-04 11:41:11 +0200

    update source code

--- app/src/main/java/com/st10449069/virtualpet/MainActivity.kt
+++ app/src/main/java/com/st10449069/virtualpet/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.st10449069.virtualpet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    var etAppName: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val etAppName = findViewById<EditText>(R.id.etAppName)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val btnNextScreen = findViewById<Button>(R.id.btnNextScreen)
+        btnNextScreen.setOnClickListener {
+            // call the next screen
+            val intent = Intent(this, MainActivity2::class.java)
+            // start next activity
+            startActivity(intent)
+        }
     }
 }
